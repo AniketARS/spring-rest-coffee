7abd0e35bc0f9bac692627c2a148bc4c97788ce7
Aniket Bharti <aniket.ars.bharti@gmail.com>
2023-01-02 20:56:54 +0530

    POJO to Entity and CrudRepository

--- src/main/java/com/aniketars/restapi/Coffee.java
+++ src/main/java/com/aniketars/restapi/Coffee.java
@@ -1,28 +1,40 @@
 package com.aniketars.restapi;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.UUID;
 
+@Entity
 class Coffee {
-    private final String id;
+
+    @Id
+    private String id;
     private String name;
+
+    public Coffee() {}
+
     public Coffee(String id, String name){
         this.id = id;
         this.name = name;
     }
 
-    public Coffee(String name){
+    public Coffee(String name) {
         this(UUID.randomUUID().toString(), name);
     }
 
     public String getId() {
         return id;
     }
+    public void setId(String id) { this.id = id; }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 }
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
